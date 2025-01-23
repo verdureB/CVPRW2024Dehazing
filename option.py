23a4dde18290f30a4996f2df4da03f3ec122c8e7
sonwe1e <sonwe1e@outlook.com>
2025-01-23 20:21:44 +0800

    更新参数解析，调整图像尺寸、批量大小和学习率等设置，添加额外数据支持和新的损失权重配置

--- option.py
+++ option.py
@@ -10,7 +10,7 @@ def parse_args():
         "--dataset_root", type=str, default="./dehaze_data_1/", help="数据集根目录"
     )
     data_group.add_argument(
-        "--image_size", type=int, default=672, help="训练时裁剪的图像尺寸"
+        "--image_size", type=int, default=336, help="训练时裁剪的图像尺寸"
     )
     data_group.add_argument(
         "--valid_image_size", type=int, default=4096, help="验证时使用的完整图像尺寸"
@@ -21,25 +21,34 @@ def parse_args():
     data_group.add_argument(
         "--ori_image_rate", type=float, default=0.0, help="使用原始清晰图像的概率"
     )
+    data_group.add_argument(
+        "--valid_image_rate", type=float, default=0.12, help="作为验证集的百分比"
+    )
+    data_group.add_argument(
+        "--crops_per_image", type=int, default=6, help="每张图像裁剪的次数"
+    )
+    data_group.add_argument(
+        "--extra_data", help="是否使用额外数据集", action="store_true"
+    )
 
     # 训练设置
     training_group = parser.add_argument_group(
         "训练设置", "控制模型训练过程的参数，例如优化器和学习率"
     )
     training_group.add_argument(
-        "-wd", "--weight_decay", type=float, default=5e-2, help="权重衰减率"
+        "-wd", "--weight_decay", type=float, default=1e-8, help="权重衰减率"
     )
     training_group.add_argument(
         "-lr", "--learning_rate", type=float, default=1e-4, help="初始学习率"
     )
     training_group.add_argument(
-        "-bs", "--batch_size", type=int, default=2, help="批量大小"
+        "-bs", "--batch_size", type=int, default=8, help="批量大小"
     )
     training_group.add_argument(
-        "-e", "--epochs", type=int, default=800, help="训练总轮数"
+        "-e", "--epochs", type=int, default=5000, help="训练总轮数"
     )
     training_group.add_argument(
-        "--pct_start", type=float, default=0.3, help="学习率预热的比例"
+        "--pct_start", type=float, default=0.06, help="学习率预热的比例"
     )
     training_group.add_argument(
         "--decay_mode", type=str, default="cos", help="学习率衰减模式 (cos, linear 等)"
@@ -48,7 +57,7 @@ def parse_args():
         "--num_workers", type=int, default=16, help="数据加载器使用的线程数"
     )
     training_group.add_argument(
-        "--beta2", type=float, default=0.999, help="AdamW 优化器的 beta2 参数"
+        "--beta2", type=float, default=0.95, help="AdamW 优化器的 beta2 参数"
     )
 
     # 损失函数配置
@@ -59,13 +68,16 @@ def parse_args():
         "--msssim_rate", type=float, default=0.0, help="MS-SSIM 损失的权重"
     )
     loss_group.add_argument(
-        "--lpips_rate", type=float, default=0.0, help="LPIPS 损失的权重"
+        "--lpips_rate", type=float, default=0.1, help="LPIPS 损失的权重"
     )
     loss_group.add_argument(
-        "--gan_g_rate", type=float, default=0.0, help="生成器的 GAN 损失权重"
+        "--gan_g_rate", type=float, default=0.001, help="生成器的 GAN 损失权重"
     )
     loss_group.add_argument(
-        "--gan_d_rate", type=float, default=0.0, help="判别器的 GAN 损失权重"
+        "--gan_d_rate", type=float, default=0.0001, help="判别器的 GAN 损失权重"
+    )
+    loss_group.add_argument(
+        "--dnet_net", type=str, default="dinov2_vits14", help="判别器的 GAN 网络类型"
     )
     loss_group.add_argument(
         "--lpips_net", type=str, default="dinov2_vits14", help="使用的 LPIPS 网络类型"
@@ -91,13 +103,16 @@ def parse_args():
         "-d", "--devices", type=int, default=5, help="使用的 GPU 设备 ID"
     )
     experiment_group.add_argument(
-        "--exp_name", type=str, default="v1->ema", help="实验名称"
+        "--exp_name",
+        type=str,
+        default="v3->cautiou+dpath0.2+dropout0.2+extra_data+cc",
+        help="实验名称",
     )
     experiment_group.add_argument(
         "--val_check", type=float, default=1.0, help="验证频率 (多少个 epoch 验证一次)"
     )
     experiment_group.add_argument(
-        "--log_step", type=int, default=10, help="日志记录频率 (多少个 batch 记录一次)"
+        "--log_step", type=int, default=25, help="日志记录频率 (多少个 batch 记录一次)"
     )
 
     return parser.parse_args()
